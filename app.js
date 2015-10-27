5c622125c5d8c2abf1ffb1157007468c74560a54
kiriharat <kiriharat@gmail.com>
2015-10-27 17:29:30 +0900

    Nodejs＋expressのHelloWorldプログラムコミット

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res){
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(1337, "127.0.0.1");
+app.get('/', function(req, res){
+    res.send('Hello World!\n');
+});
 
-console.log('Server running at http://127.0.0.1:1337/');
+app.listen(3000);
